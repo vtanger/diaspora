75e84002df9b20d9bcc1ea050ec119f0757c9964
Steven Hancock <stevenh512@gmail.com>
2012-06-16 17:11:13 -0700

    Strict Mass-Assignment checking
    Update models, controllers and specs to be compatible with strict
    Mass-Assignment checking.
    
    All cukes are passing for me locally, but there are still some specs
    that need to be fixed.

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -29,12 +29,16 @@ describe Photo do
   describe "protected attributes" do
     it "doesn't allow mass assignment of person" do
       @photo.save!
-      @photo.update_attributes(:author => Factory(:person))
+      expect {
+        @photo.update_attributes(:author => Factory(:person))
+      }.should raise_error ActiveModel::MassAssignmentSecurity::Error
       @photo.reload.author.should == @user.person
     end
     it "doesn't allow mass assignment of person_id" do
       @photo.save!
-      @photo.update_attributes(:author_id => Factory(:person).id)
+      expect {
+        @photo.update_attributes(:author_id => Factory(:person).id)
+      }.should raise_error ActiveModel::MassAssignmentSecurity::Error
       @photo.reload.author.should == @user.person
     end
     it 'allows assignment of text' do
